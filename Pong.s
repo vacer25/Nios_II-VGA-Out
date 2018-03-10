5ed453da331aac7f748a3d1998642604fa9fdbda
vacer25 <vacer25@gmail.com>
2018-03-09 20:14:53 -0500

    Added some more comments, corrected some names
    Also updated readme

--- Pong.s
+++ Pong.s
@@ -172,7 +172,7 @@
 	movi	r4, \x
     mov		r5, \y
     movui	r6, \col
-    call 	DrawDRAW_PADDLE
+    call 	DrawPaddle
 .endm
 
 # -------------------- INTERRUPTS --------------------
@@ -251,13 +251,13 @@ _start:
     movia	r3, BUTTONS_BASE_ADDR		# Buttons I/O Address
     movi 	r15, 255					# Number of NOP delays
     
-    movi 	r14, HEIGHT-P_HEIGHT		# Midline X coordinate
-    movi	r22, HEIGHT/2-P_HEIGHT/2 	# DRAW_PADDLE 1 (Left)  in middle
-    movi	r23, HEIGHT/2-P_HEIGHT/2 	# DRAW_PADDLE 2 (Right) in middle
-    movi	r18, WIDTH/2				# Ball X Pos in middle
-    movi	r19, HEIGHT/2				# Ball Y Pos in middle
-    movi 	r20, 1						# Ball X dir
-    movi 	r21, 1						# Ball Y dir
+    movi 	r14, HEIGHT-P_HEIGHT		# Midline X coordinate in middle
+    movi	r22, HEIGHT/2-P_HEIGHT/2 	# Left paddle  Y coordinate in middle
+    movi	r23, HEIGHT/2-P_HEIGHT/2 	# Right paddle Y coordinate in middle
+    movi	r18, WIDTH/2				# Ball X coordinate in middle
+    movi	r19, HEIGHT/2				# Ball Y coordinate in middle
+    movi 	r20, 1						# Ball X direction
+    movi 	r21, 1						# Ball Y direction
     
     # Setup interval timer
     movia 	r13, TIMER_BASE_ADDR		# Internal timer base address
@@ -527,7 +527,7 @@ CHAR_MAP:		.word 0x3E223E00	# 0
 # r4: x
 # r5: y
 # r6: char
-# r7: color value
+# r7: colour value
 DrawBigNumber:
 	subi 	sp, sp, 44
     stw 	r4,  0(sp)
@@ -562,17 +562,17 @@ DrawBigNumber:
 		LoopY:  			
         	bge		r17, r18, Skip_Draw				# If current Y counter > Char weight, skip draw
 			and		r5, r19, r20					# Check if current bit is set
-            beq		r5, r0, Draw_BG_Col				# If current bit is 0, skip draw
-            DRAW_PIXEL	r14, r15, r7				# Draw the pixel in the specified color
+            beq		r5, r0, Draw_BG_Col				# If current bit is 0, draw the background colour
+            DRAW_PIXEL	r14, r15, r7				# Draw the pixel in the specified colour
             br		Skip_Draw			
         Draw_BG_Col:			
-        	DRAW_PIXEL	r14, r15, r21				# Draw the pixel in the background color
+        	DRAW_PIXEL	r14, r15, r21				# Draw the pixel in the background colour
         Skip_Draw:			
         	srli	r19, r19, 1						# Shift bitmask right
-            subi 	r15, r15, 1						# Decrement Y pos
+            subi 	r15, r15, 1						# Decrement Y coordinate
             subi 	r17, r17, 1						# Decrement Y counter
             bge 	r17, r0, LoopY					# Loop if Y counter >= 0
-        addi 	r14, r14, 1							# Increment X pos
+        addi 	r14, r14, 1							# Increment X coordinate
         subi 	r16, r16, 1							# Decrement X counter
         bge 	r16, r0, LoopX						# Loop if X counter >= 0
     
@@ -589,22 +589,23 @@ DrawBigNumber:
     ldw 	r4,  0(sp)  
     addi 	sp, sp, 44
     ret
-    
+  
+  
 # r4: x
 # r5: y
 # r6: colour value
-DrawDRAW_PADDLE:
-	subi sp, sp, 40
-    stw r4,  0(sp)
-    stw r5,  4(sp)
-    stw r6,  8(sp)
-    stw r7,  12(sp)
-    stw r16, 16(sp)
-    stw r17, 20(sp)
-    stw r18, 24(sp)
-    stw r19, 28(sp)
-    stw r20, 32(sp)
-    stw ra,  36(sp)
+DrawPaddle:
+	subi 	sp, sp, 40
+    stw 	r4,  0(sp)
+    stw 	r5,  4(sp)
+    stw 	r6,  8(sp)
+    stw 	r7,  12(sp)
+    stw 	r16, 16(sp)
+    stw 	r17, 20(sp)
+    stw 	r18, 24(sp)
+    stw 	r19, 28(sp)
+    stw 	r20, 32(sp)
+    stw 	ra,  36(sp)
     
 	mov 	r16, r4
     mov 	r17, r4
@@ -614,195 +615,208 @@ DrawDRAW_PADDLE:
     addi 	r19, r19, P_HEIGHT
     mov 	r20, r6
 
-    1:	mov		r4, r18
+    1:  mov		r4, r18
         mov		r5, r19
         mov		r6, r16
         mov		r7, r20
         call 	DrawVLine
-        addi r16, r16, 1
-        blt r16, r17, 1b
+        addi 	r16, r16, 1
+        blt 	r16, r17, 1b
         
-	ldw ra,  36(sp)
-    ldw r20, 32(sp)
-    ldw r19, 28(sp)
-    ldw r18, 24(sp)
-    ldw r17, 20(sp)
-    ldw r16, 16(sp)
-    ldw r7,  12(sp)  
-    ldw r6,  8(sp)
-    ldw r5,  4(sp)
-    ldw r4,  0(sp)  
-    addi sp, sp, 40
+	ldw 	ra,  36(sp)
+    ldw 	r20, 32(sp)
+    ldw 	r19, 28(sp)
+    ldw 	r18, 24(sp)
+    ldw 	r17, 20(sp)
+    ldw 	r16, 16(sp)
+    ldw 	r7,  12(sp)  
+    ldw 	r6,  8(sp)
+    ldw 	r5,  4(sp)
+    ldw 	r4,  0(sp)  
+    addi	sp, sp, 40
     ret
 
+	
 # r4: x1
 # r5: x2
 # r6: y
 # r7: colour value
 DrawHLine:
-	subi sp, sp, 24
-    stw r4,  0(sp)
-    stw r5,  4(sp)
-    stw r6,  8(sp)
-    stw r16, 12(sp)
-    stw r17, 16(sp)
-    stw ra,  20(sp)
+	subi 	sp, sp, 24
+    stw 	r4,  0(sp)
+    stw 	r5,  4(sp)
+    stw 	r6,  8(sp)
+    stw 	r16, 12(sp)
+    stw 	r17, 16(sp)
+    stw 	ra,  20(sp)
     
-    mov  r17, r4		# x1
-    mov  r16, r5		# x2
-    mov  r5, r6			# y
-    mov  r6, r7			# Colour
+	mov  	r17, r4									# x1
+    mov  	r16, r5									# x2
+    mov  	r5, r6									# y
+    mov  	r6, r7									# Colour
     
-    1:	mov r4, r16
-        call WritePixel		# Draw one pixel
-        subi r16, r16, 1
-        bge r16, r17, 1b
+    1:	mov 	r4, r16
+        call 	WritePixel							# Draw one pixel
+        subi	r16, r16, 1
+        bge 	r16, r17, 1b
     
-    ldw ra,  20(sp)
-    ldw r17, 16(sp)  
-    ldw r16, 12(sp)  
-    ldw r6,  8(sp)
-    ldw r5,  4(sp)
-    ldw r4,  0(sp)   
-    addi sp, sp, 24
+    ldw 	ra,  20(sp)
+    ldw 	r17, 16(sp)  
+    ldw 	r16, 12(sp)  
+    ldw 	r6,  8(sp)
+    ldw 	r5,  4(sp)
+    ldw 	r4,  0(sp)   
+    addi	sp, sp, 24
     ret
 
+	
 # r4: y1
 # r5: y2
 # r6: x
 # r7: colour value
 DrawVLine:
-	subi sp, sp, 24
-    stw r4,  0(sp)
-    stw r5,  4(sp)
-    stw r6,  8(sp)
-    stw r16, 12(sp)
-    stw r17, 16(sp)
-    stw ra,  20(sp)
+	subi	sp, sp, 24
+    stw 	r4,  0(sp)
+    stw 	r5,  4(sp)
+    stw 	r6,  8(sp)
+    stw 	r16, 12(sp)
+    stw 	r17, 16(sp)
+    stw 	ra,  20(sp)
     
-    mov  r17, r4		# y1
-    mov  r16, r5		# y2
-    mov  r4, r6			# x
-    mov  r6, r7			# Colour
+    mov  	r17, r4									# y1
+    mov  	r16, r5									# y2
+    mov  	r4, r6									# x
+    mov  	r6, r7									# Colour
     
-    1:	mov r5, r16
-        call WritePixel		# Draw one pixel
-        subi r16, r16, 1
-        bge r16, r17, 1b
+    1:	mov 	r5, r16
+        call 	WritePixel							# Draw one pixel
+        subi	r16, r16, 1
+        bge 	r16, r17, 1b
     
-    ldw ra,  20(sp)
-    ldw r17, 16(sp)  
-    ldw r16, 12(sp)
-    ldw r6,  8(sp)
-    ldw r5,  4(sp)
-    ldw r4,  0(sp)   
-    addi sp, sp, 24
+    ldw 	ra,  20(sp)
+    ldw 	r17, 16(sp)  
+    ldw 	r16, 12(sp)
+    ldw 	r6,  8(sp)
+    ldw 	r5,  4(sp)
+    ldw 	r4,  0(sp)   
+    addi	sp, sp, 24
     ret
     
+	
 # r4: y1
 # r5: y2
 # r6: x
 # r7: colour value
 DrawDVLine:
-	subi sp, sp, 28
-    stw r4,  0(sp)
-    stw r5,  4(sp)
-    stw r6,  8(sp)
-    stw r16, 12(sp)
-    stw r17, 16(sp)
-    stw r18, 20(sp)
-    stw ra,  24(sp)
+	subi 	sp, sp, 28
+    stw 	r4,  0(sp)
+    stw 	r5,  4(sp)
+    stw 	r6,  8(sp)
+    stw 	r16, 12(sp)
+    stw 	r17, 16(sp)
+    stw 	r18, 20(sp)
+    stw 	ra,  24(sp)
     
-    mov  r17, r4		# y1
-    mov  r16, r5		# y2
-    mov  r4, r6			# x
-    mov  r6, r7			# Colour
+    mov  	r17, r4									# y1
+    mov  	r16, r5									# y2
+    mov  	r4, r6									# x
+    mov  	r6, r7									# Colour
     
     1:	mov 	r5, r16
     	andi	r18, r5, 1
         bne		r18, r0, Skip_Draw_Pixel
-        call 	WritePixel		# Draw one pixel
+        call 	WritePixel							# Draw one pixel
 Skip_Draw_Pixel:
         subi 	r16, r16, 1
         bge 	r16, r17, 1b
     
-    ldw ra,  24(sp)
-    ldw r18, 20(sp) 
-    ldw r17, 16(sp)  
-    ldw r16, 12(sp)
-    ldw r6,  8(sp)
-    ldw r5,  4(sp)
-    ldw r4,  0(sp)   
-    addi sp, sp, 28
+    ldw 	ra,  24(sp)
+    ldw 	r18, 20(sp) 
+    ldw 	r17, 16(sp)  
+    ldw 	r16, 12(sp)
+    ldw 	r6,  8(sp)
+    ldw 	r5,  4(sp)
+    ldw 	r4,  0(sp)   
+    addi 	sp, sp, 28
     ret    
 
+	
 # r4: colour
-FillColour:
-	subi sp, sp, 28
-    stw r4,  0(sp)
-    stw r5,  4(sp)
-    stw r6,  8(sp)
-    stw r16, 12(sp)
-    stw r17, 16(sp)
-    stw r18, 20(sp)
-    stw ra,  24(sp)
-    
-    mov r18, r4
-    
-    # Two loops to draw each pixel
-    movi r16, WIDTH-1
-    1:	movi r17, HEIGHT-1
-        2:  mov r4, r16
-            mov r5, r17
-            mov r6, r18
-            call WritePixel		# Draw one pixel
-            subi r17, r17, 1
-            bge r17, r0, 2b
-        subi r16, r16, 1
-        bge r16, r0, 1b
-    
-    ldw ra,  24(sp)
-    ldw r18, 20(sp)
-    ldw r17, 16(sp)
-    ldw r16, 12(sp)  
-    ldw r6,  8(sp)
-    ldw r5,  4(sp)
-    ldw r4,  0(sp)  
-    addi sp, sp, 28
+FillColourFast:
+	subi 	sp, sp, 12
+    stw 	r8,  0(sp)
+    stw 	r9,  4(sp)
+    stw 	r10, 8(sp)
+
+.if LOG2_BYTES_PER_PIXEL == 0
+    andi 	r8, r4, 0xff
+    slli 	r9, r8, 8
+    or   	r8, r8, r9
+    slli 	r9, r8, 16
+    or   	r8, r9, r8
+.elseif LOG2_BYTES_PER_PIXEL == 1
+    andi 	r8, r4, 0xffff
+    slli 	r9, r8, 16
+	or   	r8, r9, r8
+.elseif LOG2_BYTES_PER_PIXEL == 2
+    # Don't need to do anything
+.else
+    .error "Error: Unknown pixel size"
+.endif
+	
+	movia 	r9, (BYTES_PER_ROW)*HEIGHT-4
+	movia 	r10, PIXBUF
+	add   	r9, r9, r10
+
+# This is a temporary label!! You can do this!! :)
+1:
+    stwio 	r8, 0(r9)
+    subi  	r9, r9, 4
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    bge   	r9, r10, 1b
+
+    ldw 	r8,  0(sp)
+    ldw 	r9,  4(sp)
+    ldw 	r10, 8(sp)
+    addi 	sp, sp, 12
     ret
 
-
+	
 ClearScreen:
-	subi sp, sp, 24
-    stw r4,  0(sp)
-    stw r5,  4(sp)
-    stw r6,  8(sp)
-    stw r16, 12(sp)
-    stw r17, 16(sp)
-    stw ra,  20(sp)
+	subi 	sp, sp, 24
+    stw 	r4,  0(sp)
+    stw 	r5,  4(sp)
+    stw 	r6,  8(sp)
+    stw 	r16, 12(sp)
+    stw 	r17, 16(sp)
+    stw 	ra,  20(sp)
     
-    mov r4, r0
-    call FillColour
+    FILL_COLOR	BG_COL
     
     # Two loops to draw each char
-    movi r16, WIDTH-1
-    1:	movi r17, HEIGHT-1
-        2:  mov r4, r16
-            mov r5, r17
-            mov r6, r0
-            call WriteChar		# Draw one char
-            subi r17, r17, 1
-            bge r17, r0, 2b
-        subi r16, r16, 1
-        bge r16, r0, 1b
+    movi 	r16, WIDTH-1
+    1:	movi 	r17, HEIGHT-1
+        2:  mov 	r4, r16
+            mov 	r5, r17
+            mov 	r6, r0
+            call 	WriteChar						# Draw one char
+            subi 	r17, r17, 1
+            bge 	r17, r0, 2b
+        subi 	r16, r16, 1
+        bge 	r16, r0, 1b
     
-    ldw ra,  20(sp)
-    ldw r17, 16(sp)
-    ldw r16, 12(sp)  
-    ldw r6,  8(sp)
-    ldw r5,  4(sp)
-    ldw r4,  0(sp)  
-    addi sp, sp, 24
+    ldw 	ra,  20(sp)
+    ldw 	r17, 16(sp)
+    ldw 	r16, 12(sp)  
+    ldw 	r6,  8(sp)
+    ldw 	r5,  4(sp)
+    ldw 	r4,  0(sp)  
+    addi 	sp, sp, 24
     ret
 
 
@@ -810,21 +824,21 @@ ClearScreen:
 # r5: row
 # r6: character
 WriteChar:
-	subi sp, sp, 12
-    stw r4, 0(sp)		# Save some registers
-    stw r5, 4(sp)
-    stw r6, 8(sp)
+	subi 	sp, sp, 12
+    stw 	r4, 0(sp)
+    stw 	r5, 4(sp)
+    stw 	r6, 8(sp)
     
-	slli r5, r5, 7
-    add r5, r5, r4
-    movia r4, CHARBUF
-    add r5, r5, r4
-    stbio r6, 0(r5)
+	slli 	r5, r5, 7
+    add 	r5, r5, r4
+    movia 	r4, CHARBUF
+    add 	r5, r5, r4
+    stbio 	r6, 0(r5)
     
-    ldw r6, 8(sp)
-    ldw r5, 4(sp)
-    ldw r4, 0(sp)    
-    addi sp, sp, 12
+    ldw 	r6, 8(sp)
+    ldw 	r5, 4(sp)
+    ldw 	r4, 0(sp)    
+    addi	sp, sp, 12
     ret
 
 
@@ -832,89 +846,34 @@ WriteChar:
 # r5: row (y)
 # r6: colour value
 WritePixel:
-	subi sp, sp, 20
-    stw r2, 0(sp)		# Save some registers
-    stw r3, 4(sp)
-    stw r4, 8(sp)
-    stw r5, 12(sp)
-    stw r6, 16(sp)
+	subi 	sp, sp, 20
+    stw 	r2, 0(sp)
+    stw 	r3, 4(sp)
+    stw 	r4, 8(sp)
+    stw 	r5, 12(sp)
+    stw 	r6, 16(sp)
     
-    slli r5, r5, LOG2_BYTES_PER_ROW
-    slli r4, r4, LOG2_BYTES_PER_PIXEL
-    add r5, r5, r4
-    movia r4, PIXBUF
-    add r5, r5, r4
+    slli 	r5, r5, LOG2_BYTES_PER_ROW
+    slli 	r4, r4, LOG2_BYTES_PER_PIXEL
+    add 	r5, r5, r4
+    movia 	r4, PIXBUF
+    add 	r5, r5, r4
     
 .if LOG2_BYTES_PER_PIXEL == 0
-  	stbio r6, 0(r5)		# Write 8-bit pixel
+  	stbio 	r6, 0(r5)								# Write 8-bit pixel
 .elseif LOG2_BYTES_PER_PIXEL == 1
-    sthio r6, 0(r5)		# Write 16-bit pixel
+    sthio 	r6, 0(r5)								# Write 16-bit pixel
 .elseif LOG2_BYTES_PER_PIXEL == 2
-    stwio r6, 0(r5)		# Write 16-bit pixel
+    stwio 	r6, 0(r5)								# Write 32-bit pixel
 .else
     .error "Unknown pixel size"
 .endif
     
-    ldw r6, 16(sp)
-    ldw r5, 12(sp)
-    ldw r4, 8(sp)
-    ldw r3, 4(sp)
-    ldw r2, 0(sp)  
-    addi sp, sp, 20
-    ret
-    
-1:	sthio r6, 0(r5)		# Write 16-bit pixel
-
-	ldw r6, 16(sp)
-    ldw r5, 12(sp)
-    ldw r4, 8(sp)
-    ldw r3, 4(sp)
-    ldw r2, 0(sp)  
-    addi sp, sp, 20
-	ret
-
-FillColourFast:
-	subi sp, sp, 12
-    stw r8,  0(sp)
-    stw r9,  4(sp)
-    stw r10, 8(sp)
-
-
-.if LOG2_BYTES_PER_PIXEL == 0
-    andi r8, r4, 0xff
-    slli r9, r8, 8
-    or   r8, r8, r9
-    slli r9, r8, 16
-    or   r8, r9, r8
-.elseif LOG2_BYTES_PER_PIXEL == 1
-    andi r8, r4, 0xffff
-    slli r9, r8, 16
-	or   r8, r9, r8
-.elseif LOG2_BYTES_PER_PIXEL == 2
-    # Don't need to do anything
-.else
-    .error "Error: Unknown pixel size"
-.endif
-	
-	movia r9, (BYTES_PER_ROW)*HEIGHT-4
-	movia r10, PIXBUF
-	add   r9, r9, r10
-
-# This is a temporary label!! You can do this!! :)
-1:
-    stwio r8, 0(r9)
-    subi  r9, r9, 4
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    bge   r9, r10, 1b
-
-    ldw r8,  0(sp)
-    ldw r9,  4(sp)
-    ldw r10, 8(sp)
-    addi sp, sp, 12
+    ldw 	r6, 16(sp)
+    ldw 	r5, 12(sp)
+    ldw 	r4, 8(sp)
+    ldw 	r3, 4(sp)
+    ldw 	r2, 0(sp)  
+    addi 	sp, sp, 20
     ret
 	
--- README.md
+++ README.md
@@ -14,6 +14,9 @@ So far, the code has only been tested on the DE1-SoC board and is confirmed to b
 ## Todo:
 1. ~~Figure out double buffering~~
 	- Done
+1. Implement some form of randomness (starting ball directions, etc...)
 1. Make the score numbers the same size on different display sizes
 1. Make the ball speed constant on all display sizes
+	- In progress
 1. Interface with the SNES controller
+	- In progress
